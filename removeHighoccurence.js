c6c7372f336e17c1737d7fe1f4790a887dff53ae
irumvag <irumvagadanaclet@gmail.com>
2025-07-02 09:57:02 +0200

    kata in process

--- removeHighoccurence.js
+++ removeHighoccurence.js
@@ -1,6 +1,31 @@
-removeHighestOccurrence(array)
+function removeHighestOccurrence(array)
 {
-    
+    var acc={};
+    array.forEach(element => {
+        acc[element]=(acc[element]||0)+1;
+    });
+    console.log(acc);
+    var large=Object.keys(acc)[0];
+    var element=acc[large];
+    for(let key in acc){
+        if(acc[key]>acc[large]){
+        large=key;
+        element=acc[key]; 
+        }
+    }
+    console.log("large",large,"  element",element);
+    let arr=[];
+    array.forEach(element => {
+        if(element==large)
+        {
+
+        }
+        else
+        {
+            arr.push(element);
+        }
+    });
+    return arr;
 }
 console.log(removeHighestOccurrence([1, 2, 2, 3, 4, 4, 5])); // [1, 3, 5]
 
