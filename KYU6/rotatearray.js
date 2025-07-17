d365990a13a9f603a7c9fffb02fe9b5fbd009b45
irumvag <irumvagadanaclet@gmail.com>
2025-07-17 13:42:14 +0200

    rotate array

--- KYU6/rotatearray.js
+++ KYU6/rotatearray.js
@@ -1,24 +1,37 @@
 function rotate(array,n){
-  if(n==0) return array;
-  let res=[];
-  for(let i=0;i<n;i++)
+   if(n==0||array.length==0) return array;
+    if(n>0){
+    for(let i=0;i<n;i++)
     {
-      res.push(array[array.length-1-i]);
+      array.unshift(array.pop());
     }
+    console.log();
+    return array;
+  }else{
+    for(let i=n;i<0;i++)
+    {
+      array.push(array.shift());
+    }
+    console.log();
+    return array;
+  }
 }
+
+
+
 
 var data = [1, 2, 3, 4, 5];
 
-rotate(data, 1) // => [5, 1, 2, 3, 4]
-rotate(data, 2) // => [4, 5, 1, 2, 3]
-rotate(data, 3) // => [3, 4, 5, 1, 2]
-rotate(data, 4) // => [2, 3, 4, 5, 1]
-rotate(data, 5) // => [1, 2, 3, 4, 5]
+console.log(rotate(data, 1)); // => [5, 1, 2, 3, 4]
+console.log(rotate(data, 2)); // => [4, 5, 1, 2, 3]
+// console.log(rotate(data, 3)); // => [3, 4, 5, 1, 2]
+// console.log(rotate(data, 4)); // => [2, 3, 4, 5, 1]
+// console.log(rotate(data, 5)); // => [1, 2, 3, 4, 5]
 
-rotate(data, 0) // => [1, 2, 3, 4, 5]
+// console.log(rotate(data, 0)); // => [1, 2, 3, 4, 5]
 
-rotate(data, -1) // => [2, 3, 4, 5, 1]
-rotate(data, -2) // => [3, 4, 5, 1, 2]
-rotate(data, -3) // => [4, 5, 1, 2, 3]
-rotate(data, -4) // => [5, 1, 2, 3, 4]
-rotate(data, -5) // => [1, 2, 3, 4, 5]
+// console.log(rotate(data, -1)); // => [2, 3, 4, 5, 1]
+// console.log(rotate(data, -2)); // => [3, 4, 5, 1, 2]
+// console.log(rotate(data, -3)); // => [4, 5, 1, 2, 3]
+// console.log(rotate(data, -4)); // => [5, 1, 2, 3, 4]
+// console.log(rotate(data, -5)); // => [1, 2, 3, 4, 5]
