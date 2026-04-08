bc646ab6e613e3203084555449381c0ab172243e
Gad Anaclet Irumva <irumvagadanaclet@gmail.com>
2026-04-08 16:52:36 +0200

    Kata: adding find unique in string

--- KYU5/findunique.js
+++ KYU5/findunique.js
@@ -1,20 +1,7 @@
 function findUniq(arr) {
-  // let newarr=arr.map(a=>a.toLowerCase())
-  // let letter=newarr[0][0];
-  // return newarr.filter(a=>!a.includes(letter))[0];
-  // let sorted = [...arr].sort((a, b) => {
-  // let lowerA = a.toLowerCase();
-  // let lowerB = b.toLowerCase();
-  // return lowerA < lowerB ? -1 : lowerA > lowerB ? 1 : 0;
-  // });
-  // console.log(sorted)
-  // return sorted[0][0].toLowerCase()==sorted[1][0].toLowerCase()? sorted[sorted.length-1]: sorted[0];
-  if(arr.length==1) return arr[0];
-  let first=arr[0];
-  let hello=[...arr.slice(1)].filter(a=>{
-    return a.split(' ').filter(b=>b.split('').every(c=>!first.includes(a))).join(' ').length>0;
-  });
-  console.log(hello);
+  let unique=arr.map(a=>Array.from(new Set([...(a.toLowerCase())].sort())).join('')) // unique value and sort
+  let uniqueValue=unique.map((b,i)=>unique.lastIndexOf(b)==unique.indexOf(b)?i:'--').filter(b=>b!='--')[0]//unique value index calculate
+  return arr[uniqueValue] //return value
 }
 
 console.log(findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ])) //=== 'BbBb');
